6d0bd69d474b27278b40487427af8990c0373c68
calicanadian <ryan.stuhl@gmail.com>
2019-09-11 10:29:20 -0700

    Initial commit. Instructions included. File created.

---
+++ elevators.rb
@@ -0,0 +1,33 @@
+
+
+class Elevator
+
+  def initialize
+
+  end
+
+  def request_elevator(floor)
+
+  end
+
+  def go_to_floor(elevator, floor)
+
+  end
+
+  def report_floor(elevator, floor)
+
+  end
+
+  def open_doors(elevator)
+
+  end
+
+  def close_doors(elevator)
+
+  end
+
+  def count_trips(elevator)
+
+  end
+
+end
