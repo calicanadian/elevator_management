e3a5510a7a91b0ce14bcb842631bba3cdffdba80
calicanadian <ryan.stuhl@gmail.com>
2019-09-11 12:03:58 -0700

    Interval commit. Refactored program to use an instance variable instead of passing values.

--- elevators.rb
+++ elevators.rb
@@ -7,83 +7,85 @@ class Elevator
     @floors = (1..10).to_a
     @lift_positions = [1, 1, 1, 1]
     @lift_trips = [0, 0, 0, 0]
+    @elevator = 1
   end
 
   def request_elevator(elevator, floor)
-    elevator = elevator.nil? ? @lifts[rand(0..(@lifts.length))] : elevator
+    @elevator = elevator.nil? ? @lifts[rand(0..(@lifts.length))] : elevator
     floor = floor.to_i
     if floor < 1 || floor > 10
       puts "invalid entry, choose a floor between 1 and 10, try again."
       floor = gets.chomp
-      request_elevator(elevator, floor)
+      request_elevator(nil, floor)
     else
-      go_to_floor(elevator, @lift_positions[elevator], floor)
+      go_to_floor(@lift_positions[elevator], floor)
     end
   end
 
-  def go_to_floor(elevator, start, destination)
+  def go_to_floor(start, destination)
     if start.to_i > destination.to_i
       puts "going down"
       trip_length = start - destination
       trip_length.times do |current_floor|
         sleep(2)
-        report_floor(elevator, (start - (current_floor + 1)))
-        puts "DN: #{@lift_positions[elevator]}"
+        report_floor((start - (current_floor + 1)))
+        puts "DN: #{@lift_positions[@elevator]}"
       end
-      open_doors(elevator)
+      open_doors
     elsif start.to_i < destination.to_i
       puts "going up"
       trip_length = destination - start
       trip_length.times do |current_floor|
         sleep(2)
-        report_floor(elevator, (start + (current_floor + 1)))
-        puts "UP: #{@lift_positions[elevator]}"
+        report_floor((start + (current_floor + 1)))
+        puts "UP: #{@lift_positions[@elevator]}"
       end
-      open_doors(elevator)
+      open_doors
     else
       puts "open doors"
-      open_doors(elevator)
+      open_doors
     end
   end
 
-  def report_floor(elevator, floor)
-    @lift_positions[elevator] = (floor)
+  def report_floor(floor)
+    @lift_positions[@elevator] = (floor)
     return
   end
 
-  def open_doors(elevator)
-    puts "open doors elevator: #{elevator}"
-    puts "trip count: #{@lift_trips[elevator]}"
+  def open_doors
+    puts "open doors elevator: #{@elevator}"
+    puts "trip count: #{@lift_trips[@elevator]}"
     sleep(5)
-    close_doors(elevator)
+    close_doors
   end
 
-  def close_doors(elevator)
-    puts "close doors elevator: #{elevator}"
-    count_trips(elevator)
-    listen_for_new_request(elevator)
+  def close_doors
+    puts "close doors elevator: #{@elevator}"
+    count_trips
+    listen_for_new_request
   end
 
-  def listen_for_new_request(elevator)
+  def listen_for_new_request
     puts "Where to?"
     floor = gets.chomp
     if floor.match(/\d/)
-      request_elevator(elevator, floor.to_i)
+      request_elevator(@elevator, floor.to_i)
     else
       puts "invalid input"
-      open_doors(elevator)
+      open_doors
     end
   end
 
-  def count_trips(elevator)
-    @lift_trips[elevator] += 1
-    maintenance_check(elevator)
+  def count_trips
+    @lift_trips[@elevator] += 1
+    maintenance_check
   end
 
-  def maintenance_check(elevator)
-    if @lift_trips[elevator] == 100
-      @lifts.delete_at(elevator)
-      puts "Elevator #{elevator} is out of service for maintenance"
+  def maintenance_check
+    p @lift_trips[@elevator]
+    if @lift_trips[@elevator] == 100
+      @lifts.delete_at(@elevator)
+      puts "Elevator #{@elevator} is out of service for maintenance"
     end
   end
 
@@ -91,4 +93,4 @@ end
 
 ele = Elevator.new()
 
-ele.open_doors(1)
+ele.open_doors
