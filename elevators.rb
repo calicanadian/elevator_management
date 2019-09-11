6b15cd2ccebca23e9bc845d89d8f13267c9ffdf8
calicanadian <ryan.stuhl@gmail.com>
2019-09-11 11:34:21 -0700

    Interval commit. Able to hail an elevator, take a trip, then go to another floor.

--- elevators.rb
+++ elevators.rb
@@ -5,18 +5,20 @@ class Elevator
   def initialize
     @lifts = (1..4).to_a
     @floors = (1..10).to_a
-    @lift_positions = [[1], [1], [1], [1]]
-    @lift_trips = [[0], [0], [0], [0]]
+    @lift_positions = [1, 1, 1, 1]
+    @lift_trips = [0, 0, 0, 0]
     # puts @lifts.inspect
     # puts @floors.inspect
     # puts @lift_positions.inspect
   end
 
-  def request_elevator(floor)
-    elevator = @lifts[rand(1..4) - 1]
-    # puts "elevator: #{elevator} "
-    # puts "elevator: #{ @lift_positions[elevator - 1][0]} "
-    go_to_floor(elevator, @lift_positions[elevator - 1][0], floor)
+  def request_elevator(elevator, floor)
+    puts "elevator: #{elevator} "
+    elevator = elevator.nil? ? @lifts[rand(0..3)] : elevator
+    puts "elevator: #{elevator} "
+    puts "elevator: #{ @lift_positions[elevator]} "
+    puts "elevator: #{ @lift_positions[elevator]} "
+    go_to_floor(elevator, @lift_positions[elevator], floor)
   end
 
   def go_to_floor(elevator, start, destination)
@@ -24,21 +26,21 @@ class Elevator
       puts "going down"
       trip_length = start - destination
       trip_length.times do |current_floor|
-        # @lift_positions[0][elevator] = (start - (current_floor + 1))
+        # @lift_positions[elevator] = (start - (current_floor + 1))
         sleep(2)
-        report_floor(elevator, current_floor + 1)
-        # puts @lift_positions[elevator][0]
+        report_floor(elevator, (start - (current_floor + 1)))
+        # puts @lift_positions[elevator]
       end
       open_doors(elevator)
     elsif start.to_i < destination.to_i
       puts "going up"
-      # puts start
-      # puts destination
+      puts "Start: #{start}"
+      puts "Destination: #{destination}"
       trip_length = destination - start
       trip_length.times do |current_floor|
         sleep(2)
-        report_floor(elevator, current_floor + 1)
-        puts "UP: #{@lift_positions[0][elevator]}"
+        report_floor(elevator, (start + (current_floor + 1)))
+        puts "UP: #{@lift_positions[elevator]}"
       end
       open_doors(elevator)
     else
@@ -47,7 +49,10 @@ class Elevator
   end
 
   def report_floor(elevator, floor)
-    @lift_positions[0][elevator] = (floor)
+    @lift_positions[elevator] = (floor)
+    puts "Elevator: #{elevator}"
+    puts "Floor: #{floor}"
+    puts "Current Floor: #{@lift_positions[elevator]}"
     return
   end
 
@@ -60,6 +65,18 @@ class Elevator
 
   def close_doors(elevator)
     puts "close doors elevator: #{elevator}"
+    listen_for_new_request(elevator)
+  end
+
+  def listen_for_new_request(elevator)
+    puts "Where to?"
+    floor = gets.chomp
+    if floor.match(/\d/)
+      request_elevator(elevator, floor.to_i)
+    else
+      puts "invalid input"
+      open_doors(elevator)
+    end
   end
 
   def count_trips(elevator)
@@ -71,4 +88,4 @@ end
 
 ele = Elevator.new()
 
-ele.request_elevator(4)
+ele.request_elevator(nil, 4)
