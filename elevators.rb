82f4b74ccf4d9986f2863504f772163cb4597caf
calicanadian <ryan.stuhl@gmail.com>
2019-09-11 11:52:54 -0700

    Interval commit. Added maintenance check to program.

--- elevators.rb
+++ elevators.rb
@@ -7,18 +7,18 @@ class Elevator
     @floors = (1..10).to_a
     @lift_positions = [1, 1, 1, 1]
     @lift_trips = [0, 0, 0, 0]
-    # puts @lifts.inspect
-    # puts @floors.inspect
-    # puts @lift_positions.inspect
   end
 
   def request_elevator(elevator, floor)
-    puts "elevator: #{elevator} "
-    elevator = elevator.nil? ? @lifts[rand(0..3)] : elevator
-    puts "elevator: #{elevator} "
-    puts "elevator: #{ @lift_positions[elevator]} "
-    puts "elevator: #{ @lift_positions[elevator]} "
-    go_to_floor(elevator, @lift_positions[elevator], floor)
+    elevator = elevator.nil? ? @lifts[rand(0..(@lifts.length))] : elevator
+    floor = floor.to_i
+    if floor < 1 || floor > 10
+      puts "invalid entry, choose a floor between 1 and 10, try again."
+      floor = gets.chomp
+      request_elevator(elevator, floor)
+    else
+      go_to_floor(elevator, @lift_positions[elevator], floor)
+    end
   end
 
   def go_to_floor(elevator, start, destination)
@@ -26,16 +26,13 @@ class Elevator
       puts "going down"
       trip_length = start - destination
       trip_length.times do |current_floor|
-        # @lift_positions[elevator] = (start - (current_floor + 1))
         sleep(2)
         report_floor(elevator, (start - (current_floor + 1)))
-        # puts @lift_positions[elevator]
+        puts "DN: #{@lift_positions[elevator]}"
       end
       open_doors(elevator)
     elsif start.to_i < destination.to_i
       puts "going up"
-      puts "Start: #{start}"
-      puts "Destination: #{destination}"
       trip_length = destination - start
       trip_length.times do |current_floor|
         sleep(2)
@@ -45,26 +42,25 @@ class Elevator
       open_doors(elevator)
     else
       puts "open doors"
+      open_doors(elevator)
     end
   end
 
   def report_floor(elevator, floor)
     @lift_positions[elevator] = (floor)
-    puts "Elevator: #{elevator}"
-    puts "Floor: #{floor}"
-    puts "Current Floor: #{@lift_positions[elevator]}"
     return
   end
 
   def open_doors(elevator)
-    count_trips(elevator)
     puts "open doors elevator: #{elevator}"
+    puts "trip count: #{@lift_trips[elevator]}"
     sleep(5)
     close_doors(elevator)
   end
 
   def close_doors(elevator)
     puts "close doors elevator: #{elevator}"
+    count_trips(elevator)
     listen_for_new_request(elevator)
   end
 
@@ -80,12 +76,19 @@ class Elevator
   end
 
   def count_trips(elevator)
-    puts elevator - 1
-    # @lift_trips(elevator - 1) += 1
+    @lift_trips[elevator] += 1
+    maintenance_check(elevator)
+  end
+
+  def maintenance_check(elevator)
+    if @lift_trips[elevator] == 100
+      @lifts.delete_at(elevator)
+      puts "Elevator #{elevator} is out of service for maintenance"
+    end
   end
 
 end
 
 ele = Elevator.new()
 
-ele.request_elevator(nil, 4)
+ele.open_doors(1)
