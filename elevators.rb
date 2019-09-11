99f15c23ab831d2554c88df6a234d329c87b507e
calicanadian <ryan.stuhl@gmail.com>
2019-09-11 11:16:12 -0700

    Interval commit

--- elevators.rb
+++ elevators.rb
@@ -6,37 +6,65 @@ class Elevator
     @lifts = (1..4).to_a
     @floors = (1..10).to_a
     @lift_positions = [[1], [1], [1], [1]]
-    puts @lifts.inspect
-    puts @floors.inspect
-    puts @lift_positions.inspect
+    @lift_trips = [[0], [0], [0], [0]]
+    # puts @lifts.inspect
+    # puts @floors.inspect
+    # puts @lift_positions.inspect
   end
 
   def request_elevator(floor)
     elevator = @lifts[rand(1..4) - 1]
-    puts elevator
-    go_to_floor(elevator, @lift_positions[elevator - 1], floor)
+    # puts "elevator: #{elevator} "
+    # puts "elevator: #{ @lift_positions[elevator - 1][0]} "
+    go_to_floor(elevator, @lift_positions[elevator - 1][0], floor)
   end
 
   def go_to_floor(elevator, start, destination)
-    if start > destination
+    if start.to_i > destination.to_i
       puts "going down"
-    elsif start < destination
+      trip_length = start - destination
+      trip_length.times do |current_floor|
+        # @lift_positions[0][elevator] = (start - (current_floor + 1))
+        sleep(2)
+        report_floor(elevator, current_floor + 1)
+        # puts @lift_positions[elevator][0]
+      end
+      open_doors(elevator)
+    elsif start.to_i < destination.to_i
       puts "going up"
+      # puts start
+      # puts destination
+      trip_length = destination - start
+      trip_length.times do |current_floor|
+        sleep(2)
+        report_floor(elevator, current_floor + 1)
+        puts "UP: #{@lift_positions[0][elevator]}"
+      end
+      open_doors(elevator)
     else
       puts "open doors"
     end
   end
 
   def report_floor(elevator, floor)
+    @lift_positions[0][elevator] = (floor)
+    return
   end
 
-  def self.open_doors(elevator)
+  def open_doors(elevator)
+    count_trips(elevator)
+    puts "open doors elevator: #{elevator}"
+    sleep(5)
+    close_doors(elevator)
   end
 
-  def self.close_doors(elevator)
+  def close_doors(elevator)
+    puts "close doors elevator: #{elevator}"
   end
 
   def count_trips(elevator)
+    puts elevator - 1
+    # @lift_trips(elevator - 1) += 1
   end
 
 end
